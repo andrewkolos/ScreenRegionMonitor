59fcb569bf551afed98683f01903c64066ddd704
Andrew Kolos <andrewrkolos@gmail.com>
2021-01-13 20:01:43 -0500

    Add project files.

---
+++ ScreenRegionMonitor/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace ScreenRegionMonitor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.commandTextBox = new System.Windows.Forms.RichTextBox();
+            this.captureButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.screenCheckTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.logTextBox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // commandTextBox
+            // 
+            this.commandTextBox.Location = new System.Drawing.Point(12, 27);
+            this.commandTextBox.Name = "commandTextBox";
+            this.commandTextBox.Size = new System.Drawing.Size(315, 59);
+            this.commandTextBox.TabIndex = 0;
+            this.commandTextBox.Text = "";
+            // 
+            // captureButton
+            // 
+            this.captureButton.Location = new System.Drawing.Point(12, 262);
+            this.captureButton.Name = "captureButton";
+            this.captureButton.Size = new System.Drawing.Size(75, 27);
+            this.captureButton.TabIndex = 1;
+            this.captureButton.Text = "Capture";
+            this.captureButton.UseVisualStyleBackColor = true;
+            this.captureButton.Click += new System.EventHandler(this.captureButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(132, 262);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 27);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(252, 262);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 27);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // screenCheckTimer
+            // 
+            this.screenCheckTimer.Interval = 1000;
+            this.screenCheckTimer.Tick += new System.EventHandler(this.screenCheckTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Commands";
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(12, 105);
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.Size = new System.Drawing.Size(315, 151);
+            this.logTextBox.TabIndex = 5;
+            this.logTextBox.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Log";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 301);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.captureButton);
+            this.Controls.Add(this.commandTextBox);
+            this.Name = "MainForm";
+            this.Text = "ScreenMonitor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox commandTextBox;
+        private System.Windows.Forms.Button captureButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Timer screenCheckTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox logTextBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ScreenRegionMonitor/MainForm.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Test.VisualVerification;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenRegionMonitor
+{
+    public partial class MainForm : Form
+    {
+
+        private static string CACHE_FILE_NAME = "cache.txt";
+        public Form captureForm { get; set; }
+        private Rectangle captureRegion;
+        private Snapshot captureImage;
+        private int consecutiveFailures = 0;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void SetCapture(Rectangle region)
+        {
+            captureImage = Snapshot.FromBitmap(CaptureRegion(region));
+            captureRegion = region;
+            Log("Received screen capture.");
+        }
+
+        private void captureButton_Click(object sender, EventArgs e)
+        {
+            captureForm = new CaptureForm(this);
+            captureForm.Show();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void Start()
+        {
+            screenCheckTimer.Start();
+            stopButton.Enabled = true;
+            startButton.Enabled = false;
+            commandTextBox.Enabled = false;
+            captureButton.Enabled = false;
+            consecutiveFailures = 0;
+        }
+
+        private void screenCheckTimer_Tick(object sender, EventArgs e)
+        {
+            var current = Snapshot.FromBitmap(CaptureRegion(captureRegion));
+
+            var difference = captureImage.CompareTo(current);
+            var verifier = new SnapshotColorVerifier(Color.Black, new ColorDifference());
+            difference.ToFile("joj.png", ImageFormat.Png);
+
+            Log("Checking");
+
+            if (verifier.Verify(difference) == VerificationResult.Fail)
+            {
+                consecutiveFailures++;
+                Log("Image match failed " + consecutiveFailures + " times consecutively.");
+
+                if (consecutiveFailures > 10)
+                {
+                    Log("Running commands and stopping.");
+                    RunCommands();
+                    Stop();
+                }
+            }
+            else
+            {
+                consecutiveFailures = 0;
+            }
+        }
+
+        private void RunCommands()
+        {
+            var lines = commandTextBox.Text.Split(new[] { "\n" }
+                                          , StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in lines)
+            {
+                System.Diagnostics.Process.Start("CMD.exe", "/c " + line);
+            }
+        }
+        private Bitmap CaptureRegion(Rectangle region)
+        {
+            Bitmap _img = new Bitmap(region.Width, region.Height, PixelFormat.Format32bppArgb);
+            //create graphic variable
+            Graphics g = Graphics.FromImage(_img);
+            g.CopyFromScreen(region.Left, region.Top, 0, 0, region.Size);
+
+            Clipboard.SetImage(_img);
+
+            startButton.Enabled = true;
+
+            return _img;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void Stop()
+        {
+            screenCheckTimer.Stop();
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+            commandTextBox.Enabled = true;
+            captureButton.Enabled = true;
+        }
+
+        private void Log(string text)
+        {
+            if (!string.IsNullOrWhiteSpace(logTextBox.Text))
+            {
+                logTextBox.AppendText("\r\n" + text);
+            }
+            else
+            {
+                logTextBox.AppendText(text);
+            }
+            logTextBox.ScrollToCaret();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllText(CACHE_FILE_NAME, commandTextBox.Text);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(CACHE_FILE_NAME))
+            {
+                commandTextBox.Text = File.ReadAllText(CACHE_FILE_NAME);
+            }
+        }
+    }
+}
